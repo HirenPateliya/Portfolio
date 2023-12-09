b330fc65c6004a73b764328691c307e711607189
“HirenPateliya” <pateliyahiren90@gmail.com>
2023-12-09 09:54:32 +0530

    ASDAS

--- assets/js/hero_js.js
+++ assets/js/hero_js.js
@@ -1,1 +1,11 @@
-// tsParticles.load("tsparticles",{})
+var tl = gsap.timeline();
+
+
+tl.from('.nav-link', {
+    stagger: .3,
+    duration: 2,
+    X: 20,
+    delay: 7,
+    ease: 'Expo.easeInOut',
+    opacity: 0
+})
